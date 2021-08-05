45c314bb364c7bd02f144a9ca2b3b8929c22f646
nikhilagarwal204 <anikhil428@gmail.com>
2021-08-06 00:48:43 +0530

    GigsPage Page1 UI done

--- src/GigsPage/Page1/Page1.js
+++ src/GigsPage/Page1/Page1.js
@@ -1,9 +1,174 @@
-
-
+import Container from "react-bootstrap/Container";
+import master from "../../MasterClassPage/Masterclass.json";
+import Carousel from "react-elastic-carousel";
+import { ArrowRight } from "react-bootstrap-icons";
+import { MDBCard, MDBCardBody, MDBCardImage } from "mdb-react-ui-kit";
+import "../../App.css";
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 500, itemsToShow: 1 },
+  { width: 750, itemsToShow: 2 },
+  { width: 1080, itemsToShow: 3 },
+];
 function Page1() {
   return (
     <div>
-      Anurag start from here
+      <div className="header_masterclass">
+        <Container>
+          <div className="top_masterclass">
+            <h1>ALL GIGS / LIVE PROJECTS</h1>
+            <p className="subtitle_masterclass">
+              Short, but meant to stay with you for a{" "}
+              <span className="orange_text_masterclass">long time</span>.
+            </p>
+          </div>
+        </Container>
+      </div>
+      <div className="Mastercards">
+        {master.map((carder) => (
+          <MDBCard
+            style={{
+              borderRadius: "0px",
+              marginTop: "2%",
+              marginLeft: "4%",
+              marginRight: "4%",
+              marginBottom: "4%",
+              border: "2px solid rgba(242, 108, 79, 0.6)",
+              backgroundColor: "#020312",
+            }}
+            className="card_mastercard"
+          >
+            <div className="image_card">
+              <MDBCardImage
+                style={{
+                  marginLeft: "0.5%",
+                  width: "100%",
+                  height: "14rem",
+                  paddingTop: "20px",
+                  paddingLeft: "20px",
+                  paddingRight: "20px",
+                }}
+                src={carder.course_image}
+                alt="..."
+              />
+            </div>
+            <div className="image_logo">
+              <img className="image_logo1" src="logo192.png" />
+              <img className="image_logo2" src="logo192.png" />
+            </div>
+            <MDBCardBody>
+              <div className="Course_name">{carder.course_name}</div>
+              <hr
+                className="course_line"
+                style={{ height: "0.13rem", color: "#f26c4f" }}
+              />
+              <div className="instruct_time">
+                <div className="instructor_name">
+                  {carder.course_instructor}
+                </div>
+                <div className="time_course">{carder.course_timing}</div>
+              </div>
+              <div className="post_episode">
+                <div className="instructor_post">
+                  {carder.course_instructor_post}
+                </div>
+                <div className="episode_course">{carder.course_episode}</div>
+              </div>
+              <div className="button_masterclass1">
+                <a href={"/gigs/" + carder.id}>
+                  <button
+                    style={{ padding: "8px 14px" }}
+                    type="submit"
+                    className="button_slide_new slide_right_new"
+                  >
+                    Let's go
+                    <ArrowRight
+                      style={{
+                        width: "30px",
+                        height: "30px",
+                        marginTop: "-3px",
+                      }}
+                      className="button_arrow_new"
+                    />
+                  </button>
+                </a>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        ))}
+      </div>
+      <div className="slider_mobile">
+        <Carousel breakPoints={breakPoints}>
+          {master.map((carder) => (
+            <MDBCard
+              className="mbd_card card_mastercard"
+              style={{
+                borderRadius: "0px",
+                margin: "4%",
+                border: "2px solid rgba(242, 108, 79, 0.6)",
+                backgroundColor: "#020312",
+              }}
+            >
+              <div className="image_card">
+                <MDBCardImage
+                  style={{
+                    marginLeft: "1px",
+                    width: "100%",
+                    height: "14rem",
+                    paddingTop: "20px",
+                    paddingLeft: "20px",
+                    paddingRight: "20px",
+                  }}
+                  src={carder.course_image}
+                  alt="..."
+                />
+              </div>
+              <div className="image_logo">
+                <img className="image_logo1" src="logo192.png" />
+                <img className="image_logo2" src="logo192.png" />
+              </div>
+              <MDBCardBody>
+                <div className="Course_name">{carder.course_name}</div>
+                <hr
+                  className="course_line"
+                  style={{ height: "0.13rem", color: "#f26c4f" }}
+                />
+                <div className="instruct_time">
+                  <div className="instructor_name">
+                    {carder.course_instructor}
+                  </div>
+                  <div className="time_course">{carder.course_timing}</div>
+                </div>
+                <div className="post_episode">
+                  <div className="instructor_post">
+                    {carder.course_instructor_post}
+                  </div>
+                  <div className="episode_course">{carder.course_episode}</div>
+                </div>
+                <div className="button_masterclass1">
+                  <a href={"/gigs/" + carder.id}>
+                    <button
+                      style={{ padding: "8px 14px" }}
+                      type="submit"
+                      className="button_slide_new slide_right_new"
+                    >
+                      Let's go
+                      <ArrowRight
+                        style={{
+                          width: "30px",
+                          height: "30px",
+                          marginTop: "-3px",
+                        }}
+                        className="button_arrow_new"
+                      />
+                    </button>
+                  </a>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
 }
---
+++ src/GigsPage/Page3/Page3.js
@@ -0,0 +1,12 @@
+
+
+function Page3(props) {
+    return (
+      <div>
+  
+      </div>
+    );
+  }
+  
+  export default Page3;
+  
