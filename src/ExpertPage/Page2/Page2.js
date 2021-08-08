a93a4fe8476cfcf708128c03bd422a1d16b5a997
Harsh Lohia <harsh.lohia11@gmail.com>
2021-08-08 16:50:17 +0530

    Experts page mapping done

--- src/ExpertPage/Page2/Page2.js
+++ src/ExpertPage/Page2/Page2.js
@@ -15,7 +15,8 @@ const breakPoints = [
     { width: 1080, itemsToShow: 3 }
   ];
 
-function Page2() {
+function Page2(props) {
+  const expert = expertData[props.Eid-1];
     return (
       <div>
         <div className="header_masterclass">
@@ -29,26 +30,26 @@ function Page2() {
             </Container>
         </div>
         <div>
-            {/* {<Carousel  breakPoints={breakPoints}>} */}
+          {expertData.map(expertDetails=>(
                <MDBCard className="mbd_card card_mastercard" style={{borderRadius:"0px", margin:"4%", border:"2px solid rgba(242, 108, 79, 0.6)", backgroundColor:"#020312"}}>
                     <div className="image_card"><MDBCardImage style={{marginLeft:"1px",width:"100%",height:"14rem",paddingTop:"20px",paddingLeft:"20px",paddingRight:"20px"}} src="https://www.clipartkey.com/mpngs/m/153-1539728_cartoon-person-waving-cartoon-person-png.png" alt='...' /></div>
                     <MDBCardBody>
-                    <div className="Course_name">NAME OF EXPERT</div>
+                    <div className="Course_name">{expertDetails.ExpertName}</div>
                     <p style={{fontSize: "18px", color: "#F26C4F"}}>
-                    Current designation and company
+                    {expertDetails.ExpertDesignation}
                     </p>
                     <hr className="course_line" style={{height:"0.13rem",color:"#f26c4f"}} />
                     <div className="img_arr" style={{marginLeft: "35px"}}>
-                      <img className="img_company" src="https://www.clipartkey.com/mpngs/m/153-1539728_cartoon-person-waving-cartoon-person-png.png" />
-                      <img className="img_company" src="https://www.clipartkey.com/mpngs/m/153-1539728_cartoon-person-waving-cartoon-person-png.png" />
-                      <img className="img_company" src="https://www.clipartkey.com/mpngs/m/153-1539728_cartoon-person-waving-cartoon-person-png.png" />
+                    {expertDetails.ExpertCompaniesLogo.map(
+                      companyLogo=>
+                      <img className="img_company" src={companyLogo}/>)}
                     </div>
                     <div className="button_masterclass1">
                     <a ><button style={{padding:"8px 14px"}} type="submit" className="button_slide_new slide_right_new">Let's go<ArrowRight style={{width:"30px",height:"30px", marginTop:"-3px"}} className="button_arrow_new"/></button></a>
                     </div>
                     </MDBCardBody>
                 </MDBCard>
-            {/* {</Carousel>  }   */}
+            ))}
             </div>
             <Container className="rectangle-box2" style={{marginTop:"5%"}}>
         <Row >
