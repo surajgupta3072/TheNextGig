07b6248f3b18ee21354e3f7c3d3ad442c009c3fc
nihkilagarwal204 <anikhil428@gmail.com>
2021-09-21 02:55:20 +0530

    Search Functionality in Social Learning

--- src/SocialLearningPage/Blogs.js
+++ src/SocialLearningPage/Blogs.js
@@ -5,6 +5,7 @@ import './SocialLearningPage.css';
 function Blogs(props) {
   const [allBlogs, setAllBlogs] = useState([]);
   const [readsingleblog, setReadSingleBlog] = useState(false);
+  const [searchterm, setSearchTerm] = useState("");
   
   useEffect(() => {
     var params = {
@@ -18,6 +19,16 @@ function Blogs(props) {
       }
     });
   }, []);
+
+  function searchFilter() {
+    const searchblogs = allBlogs.filter((blog)=>{
+      if(blog.BlogTopic.toLowerCase().includes(searchterm.toLowerCase()) || blog.BlogUsername.toLowerCase().includes(searchterm.toLowerCase()) 
+      || blog.BlogHashtags.toLowerCase().includes(searchterm.toLowerCase()) || blog.BlogCreds.toLowerCase().includes(searchterm.toLowerCase())) {
+        return blog;
+      }
+    })
+    setAllBlogs(searchblogs);
+  }
 
   function BlogRead(blog) {
     setReadSingleBlog([blog])
@@ -51,6 +62,9 @@ function Blogs(props) {
 
   return (
     <div>
+      <input style={{marginLeft:"2%"}}  value={searchterm} onChange={(e)=>setSearchTerm(e.target.value)} placeholder="Search Blog..." type="search"/>
+      <button onClick={searchFilter} type="submit">Search</button>
+      <br/><br/>
       <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around"}}>
         {!readsingleblog && allBlogs.map((blog)=>
           <div key={blog.BlogID} onClick={() => {if(!props.redirlog) BlogRead(blog);  else window.location.href="/login";}}>
@@ -67,10 +81,13 @@ function Blogs(props) {
         )}
       </div>
       {readsingleblog && readsingleblog.map((blog)=>
-        <div key={blog.BlogID}>
-          <h4 style={{marginTop:"1%",marginLeft:"3%"}}>{blog.BlogTopic}</h4>
-          <p style={{marginLeft:"3%"}}>{blog.BlogHashtags}</p>
-          <p style={{marginTop:"1%",marginLeft:"3%",fontSize:"14px"}}>{blog.Blog}</p>
+        <div style={{marginLeft:"3%"}} key={blog.BlogID}>
+          <h4 style={{padding:"0", margin:"0"}}>{blog.BlogTopic}</h4>
+          <h5 style={{padding:"0", margin:"0"}}>{blog.BlogUsername}</h5>
+          <h6 style={{padding:"0", margin:"0", fontWeight:"600"}}>{blog.BlogCreds}</h6>
+          <p style={{padding:"0", margin:"0"}}>{blog.BlogHashtags}</p>
+          <br/>
+          <p style={{fontSize:"18px"}}>{blog.Blog}</p>
         </div>
       )}
     </div>
--- src/SocialLearningPage/Videos.js
+++ src/SocialLearningPage/Videos.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import docClient from '../GigsPage/GigsAWS';
 
 function Videos(props) {
+  const [videoslist, setVideosList] = useState(false);
+  const [searchterm, setSearchTerm] = useState("");
+
+  function searchFilter() {
+    const searchvids = props.prop.filter((vid)=>{
+      if(vid.VideoTopic.toLowerCase().includes(searchterm.toLowerCase()) || vid.VideoUsername.toLowerCase().includes(searchterm.toLowerCase()) 
+      || vid.VideoHashtags.toLowerCase().includes(searchterm.toLowerCase()) || vid.VideoCreds.toLowerCase().includes(searchterm.toLowerCase())) {
+        return vid;
+      }
+    })
+    setVideosList(searchvids);
+  }
 
   function VideoWatched(vid) {
     var params = {
@@ -35,8 +47,10 @@ function Videos(props) {
 
   return (
     <div>
+      <input style={{marginLeft:"2%"}} value={searchterm} onChange={(e)=>setSearchTerm(e.target.value)} placeholder="Search Video..." type="search"/>
+      <button onClick={searchFilter} type="submit">Search</button>
       <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around"}}>
-        {props.prop.map((vid)=>
+        {videoslist===false && props.prop.map((vid)=>
           <div key={vid.VideoID} onClick={() => {if(props.redirlog) window.location.href="/login";}}>
             {props.redirlog ? 
               <video style={{ height: "250px", width: "350px" }} controlsList="nodownload" onContextMenu={e => e.preventDefault()}>
@@ -49,7 +63,25 @@ function Videos(props) {
             <div>
               <h5 style={{padding:"0", margin:"0"}}>{vid.VideoTopic}</h5>
               <p style={{padding:"0", margin:"0"}}>{vid.VideoHashtags}</p>
-              <br/>
+              <h5 style={{padding:"0", margin:"0"}}>{vid.VideoUsername}</h5>
+              <h6 style={{padding:"0", margin:"0"}}>{vid.VideoCreds}</h6>
+            </div>
+            <br/>
+          </div>
+        )}
+        {videoslist!==false && videoslist.map((vid)=>
+          <div key={vid.VideoID} onClick={() => {if(props.redirlog) window.location.href="/login";}}>
+            {props.redirlog ? 
+              <video style={{ height: "250px", width: "350px" }} controlsList="nodownload" onContextMenu={e => e.preventDefault()}>
+                <source src={vid.VideoLink} />
+              </video> :
+              <video onEnded={(e)=> VideoWatched(e.target.id)} id={vid.VideoID} controls style={{ height: "250px", width: "350px" }} controlsList="nodownload" onContextMenu={e => e.preventDefault()}>
+                <source src={vid.VideoLink} />
+              </video>
+            }
+            <div>
+              <h5 style={{padding:"0", margin:"0"}}>{vid.VideoTopic}</h5>
+              <p style={{padding:"0", margin:"0"}}>{vid.VideoHashtags}</p>
               <h5 style={{padding:"0", margin:"0"}}>{vid.VideoUsername}</h5>
               <h6 style={{padding:"0", margin:"0"}}>{vid.VideoCreds}</h6>
             </div>
