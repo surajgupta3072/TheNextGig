f72f971ee856724c7fd481aef5ad56077c40403a
Your Name <anuragvaibhav7@gmail.com>
2021-09-26 23:35:29 +0530

    trying to check margin with all compo except compo 4

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -13,11 +13,11 @@ function HomePage() {
       <Page1/>
        <Page2/>
       <Page3/>
-      <Page4/>
-      {/*<Page5/>
+      {/*<Page4/>*/}
+      <Page5/>
       <Page6/>
       <Page7/>
-      <Footer/> */}
+      <Footer/> 
     </div>
   );
 }
