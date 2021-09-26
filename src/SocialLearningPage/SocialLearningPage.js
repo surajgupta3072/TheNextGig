063e4140587cfa52986e17236f6f513467ac334c
Your Name <anuragvaibhav7@gmail.com>
2021-09-26 19:11:40 +0530

    masterclass page mobile view alignment

--- src/SocialLearningPage/SocialLearningPage.js
+++ src/SocialLearningPage/SocialLearningPage.js
@@ -108,7 +108,7 @@ PPS: Knwoledge increases through sharing</span></div>
                 </Col>
                 <Navbar style={{background:"rgba(255, 255, 255, 0.1)", padding:"0px", width:"100%",   marginTop: "10%"}} expand="lg">
                 <Navbar.Brand >
-                  <p  style={{marginLeft:"25%",color: "#fff", fontWeight:"700", fontSize:"24px", margin: "10px"}}>{active}</p>
+                  <p  style={{marginLeft:"35px",color: "#fff", fontWeight:"700", fontSize:"24px", margin: "10px"}}>{active}</p>
                 </Navbar.Brand>
                 <Navbar.Toggle style={{backgroundColor: "grey"}}/>
                 <Navbar.Collapse className="justify-content-end" style={{paddingRight:"5%"}}>
@@ -148,7 +148,7 @@ PPS: Knwoledge increases through sharing</span></div>
                 }
                 {active==="Blogs" &&
                 <Col>
-                  <a onClick={() => {if(!redirectlogin) setModalShow(true);  else window.location.href="/login";}} style={{cursor: "pointer"}}><p style={{fontWeight:"bold",fontSize:"16px",color:"rgba(242, 108, 79, 1)"}}>Impart knowledge + <br/>(Add Blog)</p></a>
+                  <a onClick={() => {if(!redirectlogin) setModalShow(true);  else window.location.href="/login";}} style={{cursor: "pointer"}}><p className="impart_know" style={{fontWeight:"bold",fontSize:"16px",color:"rgba(242, 108, 79, 1)"}}>Impart knowledge + <br/>(Add Blog)</p></a>
                   <MyVerticallyPopUpBlog
                     userid={user}
                     show={modalShow}
