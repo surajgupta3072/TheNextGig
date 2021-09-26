9e4eabd6fc8074464dcf949a210d1279bb9defa2
Your Name <anuragvaibhav7@gmail.com>
2021-09-27 01:34:37 +0530

    trying to update compo 4 margin issue

--- src/HomePage/Page4/Page4.js
+++ src/HomePage/Page4/Page4.js
@@ -130,10 +130,10 @@ function Page4() {
          <Container>
             <Row style={{marginTop: "5%"}}>
                 <Col className="main_col1" md={8} >
-                    <Row style={{marginTop: "15%"}} xs={10}>
-                        {/* <Row>
+                    <Row style={{marginTop: "15%"}} xs={7}>
+                        <Row>
                             <h1 style={{textAlign:"center"}} className="heading_1">Practice what you <span style={{ textDecorationLine: 'line-through', textDecorationColor: "#F26C4F"}} className="heading_1span">preach.</span></h1>
-                        </Row> */}
+                        </Row>
                          <Row>
                             <h1 className="heading_2" style={{color: "#F26C4F",textAlign:"center"}} >learn.</h1>
                         </Row> 
