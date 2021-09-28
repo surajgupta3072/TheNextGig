e0d8b8d8e4eb12b2ccc4c86099bfdc4a78a7eb60
nihkilagarwal204 <anikhil428@gmail.com>
2021-09-28 16:38:49 +0530

    GoogleSignIn done & App.js Register localstorageCheck

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Amplify from "aws-amplify";
 import config from "./config.json";
+
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+// Assuming you have two redirect URIs, and the first is for localhost and second is for production
+const [localRedirectSignIn, productionRedirectSignIn] = ["", "https://main.d2pb8vz95cq00i.amplifyapp.com/"]
+const [localRedirectSignOut, productionRedirectSignOut] = ["http://localhost:3000/", "https://main.d2pb8vz95cq00i.amplifyapp.com/"]
 
 Amplify.configure({
   Auth: {
     mandatorySignIn: true,
     region: config.cognito.REGION,
     userPoolId: config.cognito.USER_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  oauth: {
+    domain: "thenextgig.auth.ap-south-1.amazoncognito.com",
+    scope: ["email", "profile", "openid"],
+    redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+    redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+    responseType: "token", // or 'token', note that REFRESH token will only be generated when the responseType is code
   }
 });
 
@@ -20,7 +41,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
