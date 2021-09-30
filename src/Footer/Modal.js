2361e37848d8b4e06999348e19e497146b0512fe
Your Name <anuragvaibhav7@gmail.com>
2021-10-01 02:03:35 +0530

    added email service

--- src/Footer/Modal.js
+++ src/Footer/Modal.js
@@ -1,8 +1,36 @@
 import Modal from 'react-bootstrap/Modal';
 import './Modal.css';
+import emailjs,{send} from 'emailjs-com';
 import {ArrowLeft} from 'react-bootstrap-icons'
-
+import {useState} from 'react'
 function MyVerticallyCenteredModal(props) {
+  const [feedback, setFeedback] = useState("")
+  const [data,setdata]=useState("")
+const handleChange = event => {
+  setFeedback(event.target.value)
+}
+const handleid = event => {
+  setdata(event.target.value)
+}
+const SERVICE_ID="service_mztzudb"
+const TEMPLATE_ID="template_4od9vgl"
+const submit = event => {
+  event.preventDefault()
+  emailjs
+    .send(
+      SERVICE_ID,
+      TEMPLATE_ID,
+      {feedback,"Details":data},
+      "user_LuNukIHe37LdAF6nNkxao"
+    )
+    .then(res => {
+      if (res.status === 200) {
+        
+      }
+    })
+    // Handle errors here however you like
+    .catch(err => console.error("Failed to send feedback. Error: ", err))
+}
     return (
       <Modal
         {...props}
@@ -17,10 +45,10 @@ function MyVerticallyCenteredModal(props) {
          <div style={{padding:"7%"}}>
            <p style={{fontSize:"18px"}}>Email / Mobile Number <text style={{color:"#f26c4f"}}>*</text></p>
            <p style={{marginTop:"-15px",fontSize:"15px"}}>(we promise we won't spam)</p>
-           <input style={{width:"100%"}}></input>
+           <input onChange={handleid} value={data} style={{width:"100%"}}></input>
            <p style={{marginTop:"10%",fontSize:"18px"}}>What do you want to tell us? <text style={{color:"#f26c4f"}}>*</text></p>
-           <input style={{width:"100%",height:"100px"}}></input>
-           <button className="button_slide slide_right" style={{marginTop:"10%",marginLeft:"30%"}}>Submit<ArrowLeft className='button_arrow'/></button>
+           <input onChange={handleChange} value={feedback} style={{width:"100%",height:"100px"}}></input>
+           <button onClick={submit} className="button_slide slide_right" style={{marginTop:"10%",marginLeft:"30%"}}>Submit<ArrowLeft className='button_arrow'/></button>
          </div>
       </Modal.Body>  
 
--- src/MasterClassPage/Page2/popup.js
+++ src/MasterClassPage/Page2/popup.js
@@ -1,8 +1,40 @@
 import Modal from 'react-bootstrap/Modal';
 import './popup.css';
+import emailjs,{send} from 'emailjs-com';
 import {ArrowLeft} from 'react-bootstrap-icons'
-
+import {useState} from 'react'
 function MyVerticallyPopUp(props) {
+  const [data,setdata]=useState("")
+  const [field1,setfield1]=useState("")
+  const [field2,setfield2]=useState("")
+const handlefield1 = event => {
+  setfield1(event.target.value)
+}
+const handlefield2 = event => {
+  setfield2(event.target.value)
+}
+const handleid = event => {
+  setdata(event.target.value)
+}
+const SERVICE_ID="service_mztzudb"
+const TEMPLATE_ID="template_r0wbju9"
+const submit = event => {
+  event.preventDefault()
+  emailjs
+    .send(
+      SERVICE_ID,
+      TEMPLATE_ID,
+      {"title":"Masterclasses",field1,field2,"Details":data},
+      "user_LuNukIHe37LdAF6nNkxao"
+    )
+    .then(res => {
+      if (res.status === 200) {
+        
+      }
+    })
+    // Handle errors here however you like
+    .catch(err => console.error("Failed to send feedback. Error: ", err))
+}
     return (
       <Modal
         {...props}
@@ -15,18 +47,18 @@ function MyVerticallyPopUp(props) {
       >
       <Modal.Body style={{backgroundColor:"#020312", border: "1px solid #f26c4f"}}>
          <div style={{padding:"7%"}}>
-           <p style={{fontSize:"18px"}}>Email / Mobile Number <text style={{color:"#f26c4f"}}>*</text></p>
+           <p style={{fontSize:"18px"}}>Email / Mobile Number <text  style={{color:"#f26c4f"}} >*</text></p>
            <p style={{marginTop:"-15px",fontSize:"15px"}}>(we promise we won't spam)</p>
-           <input style={{width:"100%"}}></input>
+           <input onChange={handleid} value={data} style={{width:"100%"}}></input>
            <p style={{marginTop:"10%",fontSize:"18px"}}>Recommendation<text style={{color:"#f26c4f"}}>*</text></p>
            <p style={{marginTop:"-15px",fontSize:"15px"}}> (we will try our best to ____)</p>
-           <select style={{width:"100%"}}>
+           <select onChange={handlefield1} style={{width:"100%"}} value={field1}>
               <option selected disabled>Select from dropdown*</option>
               <option>Session</option>
               <option>Expert</option>
             </select>
-           <input style={{width:"100%",marginTop:"10%"}} placeholder="Insert text here*" />
-           <button className="button_slide slide_right" style={{marginTop:"10%",marginLeft:"30%"}}>Submit<ArrowLeft className='button_arrow'/></button>
+           <input value={field2} onChange={handlefield2} style={{width:"100%",marginTop:"10%"}} placeholder="Insert text here*" />
+           <button onClick={submit} className="button_slide slide_right" style={{marginTop:"10%",marginLeft:"30%"}}>Submit<ArrowLeft className='button_arrow'/></button>
          </div>
       </Modal.Body>  
 
