7e3f2b6800c1156636b3b501b53b19586fdc521d
Your Name <anuragvaibhav7@gmail.com>
2021-10-05 19:15:07 +0530

    trying to correct margin of social learning page

--- src/SocialLearningPage/SocialLearningPage.js
+++ src/SocialLearningPage/SocialLearningPage.js
@@ -103,7 +103,7 @@ function SocialLearningPage(props) {
               </div>
             </Col>
             <Col xs={12}  className="SocialLearn_list_mobile" style={{marginTop: "10%"}}>       
-              <Row >
+              {/* <Row >
                 <div className="profile_logo_text_mobile" ><div><img alt="dp" src="google_logo.jpg" style={{height:"100px",width:"100px",borderRadius:"50%"}}/></div>
                 <div>
                   {user.attributes!==undefined && <div><p style={{fontSize:"18px", textAlign:"center"}}>{user.attributes.name}</p></div>}
@@ -111,7 +111,7 @@ function SocialLearningPage(props) {
                   <div style={{textAlign:"center"}}><p style={{margin:"0"}}>Your Referral Code:</p><p style={{color:"#F26C4F"}}><b>{myrefcode}</b></p></div>
                 </div> 
                 </div>
-              </Row>
+              </Row> */}
               <br/>
               <Row style={{marginTop:"5%"}} >
                <div style={{display:"flex",justifyContent:"space-evenly"}}>
