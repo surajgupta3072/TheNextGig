b8500130f43463879b53dbd49b925827f7a53286
Your Name <anuragvaibhav7@gmail.com>
2021-10-05 21:22:32 +0530

    trying to correct margin of social learning page

--- src/SocialLearningPage/SocialLearningPage.js
+++ src/SocialLearningPage/SocialLearningPage.js
@@ -128,7 +128,7 @@ function SocialLearningPage(props) {
                   <button onClick={()=>buttonColor("Blogs")} style={{backgroundColor:color2,marginRight:"5%",color:textColor2,borderRadius:"40px",width:"120px",height:"30px",fontWeight:"bold",border:"0px"}}>Blogs</button>
                   <button onClick={()=>buttonColor("Community")} style={{backgroundColor:color3,color:textColor3,borderRadius:"40px",width:"120px",height:"30px",fontWeight:"bold",border:"0px"}}>Community</button>
                </Col>
-                {active==="Videos" &&
+                {/* {active==="Videos" &&
                 <div>
                   {<a onClick={() => {if(!redirectlogin) setModalShow(true);  else window.location.href="/login";}} style={{cursor: "pointer"}}><p className="impart_know" style={{fontWeight:"bold",fontSize:"16px",color:"rgba(242, 108, 79, 1)"}}>Impart knowledge + <br/>(Add Video)</p></a>}
                   <MyVerticallyPopUp
@@ -147,7 +147,7 @@ function SocialLearningPage(props) {
                     onHide={() => setModalShow(false)}
                   />
                 </div>
-                } 
+                }  */}
              </Row>
                 {active === "Videos" && <Videos prop={allvideos} userid={user.username} redirlog={redirectlogin}/>}
                 {active === "Blogs" && <Blogs userid={user.username} redirlog={redirectlogin}/>}
