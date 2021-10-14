511bd7a9a921c7a0759611cd217d21555d87bdaa
Your Name <anuragvaibhav7@gmail.com>
2021-10-14 16:19:30 +0530

    added search functionality and made buttons clickable in gigs page

--- src/GigsPage/Page1/Page1.js
+++ src/GigsPage/Page1/Page1.js
@@ -17,7 +17,7 @@ const breakPoints = [
 function Page1(props) {
   const [allBlogs, setAllBlogs] = useState([]);
   const [searchterm, setSearchTerm] = useState("");
-  const [active, setActive] =  useState("Videos");
+  const [active, setActive] =  useState("Jobs");
   const [color1,setColor1] =useState("#f26c4f");
   const [textColor1,setextColor1] =useState("white");
   const [color2,setColor2] =useState("white");
@@ -26,7 +26,8 @@ function Page1(props) {
   const [textColor3,setextColor3] =useState("#f26c4f");
   const [gigs, setGigs] = useState([]);
   const [redirectlogin, setRedirectLogin] = useState(false);
-
+  const [buck,setbuck]=useState(1)
+  const [videoslist, setVideosList] = useState(false);
   useEffect(() => {
     props.prop===null && setRedirectLogin(true)
     let paramss = {
@@ -41,15 +42,56 @@ function Page1(props) {
     }
     });
   }, []);
+  function addSearchTerm() {
+    var params = {
+      TableName: "UsersTable",
+      Key: { "UserID":props.userid },
+      ProjectionExpression: "VideosSearchHistory",
+    };
+    docClient.get(params, function(err, data) {
+      if (err) {
+        console.log(err);
+      } 
+      else {
+        var params = {
+          TableName: "UsersTable",
+          Key: { "UserID":props.userid },
+          UpdateExpression: "set SkillsAcquiredGigs["+data.Item.SkillsAcquiredGigs.length.toString()+"] = :vsh",
+          ExpressionAttributeValues: {
+            ":vsh": {timestamp: `${Date.now()}`, svterm: searchterm}
+          },
+          ReturnValues:"UPDATED_NEW"
+        }
+        docClient.update(params, function (err, data) {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    });
+  }
+  function searchFilter() {
+    addSearchTerm();
+    const searchvids = gigs.filter((vid)=>{
+      if(vid.CompanyIndustry.toLowerCase().includes(searchterm.toLowerCase()) || vid.GigFunction.toLowerCase().includes(searchterm.toLowerCase()) 
+      || vid.GigName.toLowerCase().includes(searchterm.toLowerCase()) || vid.GigDuration.toLowerCase().includes(searchterm.toLowerCase())) {
+        return vid;
+      }
+    })
+    setVideosList(searchvids);
+  }
   function buttonColor(word){
     setActive(word)
     if(word==="Jobs"){
+      setbuck(1);
       setColor1("#f26c4f");setextColor1("white");setColor2("white");setextColor2("#f26c4f");setColor3("white");setextColor3("#f26c4f");
     }
     if(word==="Gigs/Projects"){
+      setbuck(2);
      setColor1("white");setextColor1("#f26c4f");setColor2("#f26c4f");setextColor2("white");setColor3("white");setextColor3("#f26c4f");
    }
    if(word==="Internships"){
+    setbuck(3);
      setColor1("white");setextColor1("#f26c4f");setColor2("white");setextColor2("#f26c4f");setColor3("#f26c4f");setextColor3("white");
    }
   }
@@ -58,21 +100,31 @@ function Page1(props) {
       <div className="gigs_top_image"><Container><h1 style={{textShadow:"0px 4px 4px #F26C4F"}}>ALL OPPORTUNITIES</h1><p style={{fontFamily:"Open Sans"}}>We believe the best way to learn something is by experiencing it yourself - pick out of gigs, internships or even a job </p><p style={{fontStyle:"Open Sans",marginTop:"-10px"}}>to make your learning complete.</p></Container></div>
       <br/>
       <Container>
-      <input className="search" style={{ borderRadius:"20px", background:"white", color:"rgb(242, 108, 79)", border:"0px"}}  value={searchterm} onChange={(e)=>setSearchTerm(e.target.value)} placeholder="Search for companies / domains / profiles / etc." type="search"/>&nbsp;&nbsp;&nbsp;
-      <button className="search_button" /* onClick={searchFilter} */ style={{backgroundColor:"rgb(242, 108, 79)",color:"white",borderRadius:"40px",width:"100px",height:"30px",fontWeight:"bold",border:"0"}}>Search</button>
+      <input className="search" style={{ borderRadius:"20px", background:"white", color:"rgb(242, 108, 79)", border:"0px"}}  value={searchterm} onChange={(e)=>{setSearchTerm(e.target.value);if(e.target.value==""){setVideosList(false)}}} placeholder="Search for companies / domains / profiles / etc." type="search"/>&nbsp;&nbsp;&nbsp;
+      <button className="search_button"  onClick={searchFilter} style={{backgroundColor:"rgb(242, 108, 79)",color:"white",borderRadius:"40px",width:"100px",height:"30px",fontWeight:"bold",border:"0"}}>Search</button>
       <br/>
       <br/>
         <Row>
           <Col xs={9} className="SocialLearn_laptop">
-            <button onClick={()=>buttonColor("Jobs")} style={{marginRight:"5%",backgroundColor:color1,color:textColor1,borderRadius:"40px",width:"120px",height:"30px",fontWeight:"bold",border:"0px"}}>Jobs</button>
-            <button onClick={()=>buttonColor("Gigs/Projects")} style={{backgroundColor:color2,marginRight:"5%",color:textColor2,borderRadius:"40px",width:"120px",height:"30px",fontWeight:"bold",border:"0px"}}>Gigs/Projects</button>
+            <button onClick={()=>{buttonColor("Jobs")}} style={{marginRight:"5%",backgroundColor:color1,color:textColor1,borderRadius:"40px",width:"120px",height:"30px",fontWeight:"bold",border:"0px"}}>Jobs</button>
+            <button onClick={()=>{buttonColor("Gigs/Projects")}} style={{backgroundColor:color2,marginRight:"5%",color:textColor2,borderRadius:"40px",width:"120px",height:"30px",fontWeight:"bold",border:"0px"}}>Gigs/Projects</button>
             <button onClick={()=>{buttonColor("Internships")}} style={{backgroundColor:color3,color:textColor3,borderRadius:"40px",width:"120px",height:"30px",fontWeight:"bold",border:"0px"}}>Internships</button>
           </Col>
         </Row>
       </Container>
+      <div className="SocialLearn_list_mobile">
+      <div style={{marginTop:"5%"}} >
+                <div style={{display:"flex",justifyContent:"space-evenly"}}>
+                  <div><button onClick={()=>{buttonColor("Jobs")}} style={{backgroundColor:color1,color:textColor1,borderRadius:"40px",width:"100px",height:"30px",fontWeight:"bold",border:"0px"}}>Jobs</button></div>
+                  <div><button onClick={()=>{buttonColor("Gigs/Projects")}} style={{backgroundColor:color2,color:textColor2,borderRadius:"40px",width:"100px",height:"30px",fontWeight:"bold",border:"0px"}}>Gigs</button></div>
+                  <div><button onClick={()=>{buttonColor("Internships")}} style={{backgroundColor:color3,color:textColor3,borderRadius:"40px",width:"100px",height:"30px",fontWeight:"bold",border:"0px"}}>Internships</button></div>
+               </div> 
+             </div> 
+      </div>
       <div style={{display:"flex",justifyContent:"space-evenly"}} className="Mastercards">
-        {gigs.map((carder) => (
-          <MDBCard
+        {videoslist===false?gigs.map((carder) => {
+        if(carder.Bucket===buck)
+         return <MDBCard
             onClick={() => {if(!redirectlogin) window.location.href="/ExperientialLearning/"+carder.GigId;  else window.location.href="/login";}}
             key={carder.GigId}
             style={{
@@ -124,12 +176,67 @@ function Page1(props) {
               <div style={{display:"flex",justifyContent:"space-evenly",paddingTop:"10px"}}><em>Apply by {carder.GigApplyBy}</em></div>
             </MDBCardBody>
           </MDBCard>
-        ))}
+        }):videoslist.map((carder) => {
+          if(carder.Bucket===buck)
+           return <MDBCard
+              onClick={() => {if(!redirectlogin) window.location.href="/ExperientialLearning/"+carder.GigId;  else window.location.href="/login";}}
+              key={carder.GigId}
+              style={{
+                cursor:"pointer",
+                borderRadius: "0px",
+                marginTop: "2%",
+                height:"30rem",
+                marginBottom: "4%",
+                border: "2px solid rgba(242, 108, 79, 0.6)",
+                backgroundColor: "#020312",
+              }}
+              className="cax card_mastercard mbd_card"
+            >
+              <div className="image_card">
+                <MDBCardImage className="mbd_image"
+                  style={{
+                    marginLeft: "0.5%",
+                    width: "100%",
+                    height: "22rem"
+                  }}
+                  src={carder.GigImage}
+                  alt="..."
+                />
+              </div>
+              <MDBCardBody>
+                <div className="Course_name">{carder.GigName}</div>
+                <hr
+                  className="course_line"
+                  style={{ height: "0.13rem", color: "#f26c4f" }}
+                />
+                <div className="instruct_time">
+                  <div className="instructor_name" >
+                    {carder.GigFunction} <sup data-tip data-for={carder.GigId+"g"}>&#9432;</sup>
+                    <ReactTooltip id={carder.GigId+"g"} place="top" effect="solid">
+                      {carder.GigDescription.substring(0, 150)}...
+                    </ReactTooltip>
+                  </div>
+                  <div className="time_course">{carder.GigDuration}</div>
+                </div>
+                <div className="post_episode">
+                  <div className="instructor_post">
+                    {carder.CompanyName} <sup data-tip data-for={carder.GigId+"d"}>&#9432;</sup>
+                    <ReactTooltip id={carder.GigId+"d"} place="top" effect="solid">
+                      {carder.CompanyDescription.substring(0, 150)}...
+                    </ReactTooltip>
+                  </div>
+                  <div className="episode_course">{carder.GigStipend}</div>
+                </div>
+                <div style={{display:"flex",justifyContent:"space-evenly",paddingTop:"10px"}}><em>Apply by {carder.GigApplyBy}</em></div>
+              </MDBCardBody>
+            </MDBCard>
+          })}
       </div>
       <div className="slider_mobile">
         <Carousel breakPoints={breakPoints}>
-          {gigs.map((carder) => (
-            <MDBCard
+          {videoslist===false?gigs.map((carder) => {
+            if(carder.Bucket===buck)
+            return <MDBCard
             onClick={() => {if(!redirectlogin) window.location.href="/ExperientialLearning/"+carder.GigId;  else window.location.href="/login";}}
               key={carder.GigId}
               className="mbd_card card_mastercard"
@@ -173,7 +280,53 @@ function Page1(props) {
               <div style={{display:"flex",justifyContent:"space-evenly",paddingTop:"10px"}}>Apply by {carder.GigApplyBy}</div>
               </MDBCardBody>
             </MDBCard>
-          ))}
+}):videoslist.map((carder) => {
+  if(carder.Bucket===buck)
+  return <MDBCard
+  onClick={() => {if(!redirectlogin) window.location.href="/ExperientialLearning/"+carder.GigId;  else window.location.href="/login";}}
+    key={carder.GigId}
+    className="mbd_card card_mastercard"
+    style={{
+      cursor:"pointer",
+      borderRadius: "0px",
+      margin: "4%",
+      border: "2px solid rgba(242, 108, 79, 0.6)",
+      backgroundColor: "#020312",
+    }}
+  >
+    <div className="image_card">
+      <MDBCardImage className="mbd_image"
+        style={{
+          marginLeft: "1px",
+          width: "100%",
+          height: "14rem"
+        }}
+        src={carder.GigImage}
+        alt="..."
+      />
+    </div>
+    <MDBCardBody>
+      <div className="Course_name">{carder.GigName}</div>
+      <hr
+        className="course_line"
+        style={{ height: "0.13rem", color: "#f26c4f" }}
+      />
+      <div className="instruct_time">
+        <div className="instructor_name">
+          {carder.GigFunction}
+        </div>
+        <div className="time_course">{carder.GigDuration}</div>
+      </div>
+      <div className="post_episode">
+        <div className="instructor_post">
+          {carder.CompanyName}
+        </div>
+        <div className="episode_course">{carder.GigStipend}</div>
+      </div>
+    <div style={{display:"flex",justifyContent:"space-evenly",paddingTop:"10px"}}>Apply by {carder.GigApplyBy}</div>
+    </MDBCardBody>
+  </MDBCard>
+})}
         </Carousel>
       </div>
     </div>
