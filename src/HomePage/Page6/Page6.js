4a47b151ce8460823fbb1816c7522eec9c934ce0
Anurag15v <anuragvaibhav7@gmail.com>
2021-12-17 01:16:47 +0530

    added stats text correctly

--- src/HomePage/Page6/Page6.js
+++ src/HomePage/Page6/Page6.js
@@ -8,7 +8,7 @@ function Page6() {
                 <div className="numbers_page6">
                     <h1 className="client_number"><span className="number1"></span><BsSlash className="liner_page6 liner_1" /><h5 style={{ fontSize: "1.1rem", color: "white", marginLeft: "32px" }}>Minutes<br />spent</h5></h1>
                     <h1 className="projects_number"><span className="number2"></span><BsSlash className="liner_page6 liner_2" /><h5 style={{ fontSize: "1.1rem", color: "white", textAlign: "center", marginLeft: "-55%" }}>Months of <br /> Experiential Learning</h5></h1>
-                    <h1 className="awards_number" ><span className="number3"></span><BsSlash className="liner_page6 liner_3" /><h5 style={{ fontSize: "1.1rem", color: "white", textAlign: "center", marginLeft: "-55%" }}>Minutes of <br /> available content <br /> content</h5></h1>
+                    <h1 className="awards_number" ><span className="number3"></span><BsSlash className="liner_page6 liner_3" /><h5 style={{ fontSize: "1.1rem", color: "white", textAlign: "center", marginLeft: "-55%" }}>Minutes of <br /> available <br /> content</h5></h1>
                     <h1 className="experience_number"><span className="number4"></span><h5 style={{ fontSize: "1.1rem", color: "white", marginTop: "4px", textAlign: "center", marginLeft: "-40%" }}>Connections made<br />for opportunities</h5></h1>
                 </div>
                 <div className="numbers_page6_mobile">
