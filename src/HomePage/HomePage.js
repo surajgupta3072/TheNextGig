19e2544999c48c993ace5c4f512f6df969d5ceb4
Anurag15v <anurag@inteliyo.net>
2022-01-31 12:04:17 +0530

    fb link

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -4,11 +4,16 @@ import Page6 from './Page6/Page6';
 import Page7 from './Page7/Page7';
 import Footer from '../Footer/Footer';
 import { useParams } from "react-router-dom";
+import { useEffect } from 'react';
 
 function HomePage(props) {
   console.log(props)
   const { id } = useParams();
-  console.log(id);
+  useEffect(() => {
+    if (window.location.href.split("/")[3].includes("?fbclid=")) {
+      window.location.href = "https://www.thenextgig.net";
+    }
+  }, [])
 
   return (
     <div>
